9e88130537162432cd8ed9736443a9465f3ee5dc
Emmydantoch <emmanueltdaniel@yahoo.com>
2025-08-14 11:42:45 +0100

    rendenr

--- languge_companion/settings.py
+++ languge_companion/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!g!it7%wl7#@s78guy++0rp$=n(h6nyy)tyhf^^_h*v8_p(_gt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['languge-companion.onrender.com', '.onrender.com', 'localhost', '127.0.0.1']
 
@@ -122,9 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional: for custom static files
-STATIC_ROOT = BASE_DIR / "staticfiles"    # For collecting static files
+STATIC_URL = "/static/"
+
+# Static files configuration for Render deployment
+if os.environ.get('RENDER'):
+    # Production settings for Render
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = []  # Don't use custom static dirs on Render
+else:
+    # Development settings
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
